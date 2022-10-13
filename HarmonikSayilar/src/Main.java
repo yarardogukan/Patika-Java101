946fa9317fd22c6055ece11f7084088d974e77d9
yarardogukan <yarardogukan@gmail.com>
2022-10-13 11:08:36 +0300

    Harmonik sayıyı hesaplayan program
    Java ile girilen sayının harmonik serisini bulan program yazacağız.
    
    Harmonik Seri Formülü :
    
    1 + 1/2 + 1/3 + 1/4 + ... + 1/n

---
+++ HarmonikSayilar/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N Sayısını Giriniz: ");
+        int n = input.nextInt();
+
+        double result=0.0;
+
+        for (double i=1; i <= n; i++)
+        {
+            result+=(1/i);
+        }
+        System.out.println("Cevap: " + result);
+
+    }
+}
